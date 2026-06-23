de836fa903f692877469040684c2092854c2a2dc
Dorian-Raud <dorian.raud@gmail.com>
2026-06-23 13:53:33 +0200

    Fix : video hero section

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -4,13 +4,21 @@ import styles from "./Hero.module.css";
 export default function Hero() {
   return (
     <section id="top" className={styles.hero}>
-      <Image
+      {/* <Image
         src="/heroSectionImg.jpg"
         alt="Œuvre exposée à la galerie"
         fill
         priority
         className={styles.image}
-      />
+      /> */}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline>
+        <source src="/78C67E0B-5B2D-404E-8D6B-BB4D0B994B11.mov" type="video/mp4" />
+
+      </video>
       <div className={styles.overlay} />
       <div className={styles.content}>
         <p className={styles.eyebrow}>Galerie d&apos;art contemporain</p>
